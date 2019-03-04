ec64d9a279638a7c1778782158431a1d96260e1d
Md Nazish <mdnazish.edu@gmail.com>
2019-03-04 12:59:56 +0530

    Project Setup is ready.

---
+++ LoginRegisterApp2/src/main/java/com/mn/app/LoginRegisterApp2Application.java
@@ -0,0 +1,13 @@
+package com.mn.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegisterApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRegisterApp2Application.class, args);
+	}
+
+}
---
+++ LoginRegisterApp2/src/test/java/com/mn/app/LoginRegisterApp2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mn.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoginRegisterApp2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
